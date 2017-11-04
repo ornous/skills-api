1a3d636dc3f0e8f33350cd2761a83d1eb14dbe35
Ousmane Ndiaye (Ozzy) <snekshaark@gmail.com>
2017-11-04 15:00:03 +0000

    Resolve "Add GraphQL Subscriptions capability"

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ require('dotenv').config()
 const bunyan = require('bunyan')
 const bodyParser = require('body-parser')
 const compression = require('compression')
-const Engine = require('apollo-engine').Engine
+const { Engine } = require('apollo-engine')
 const expressBunyan = require('express-bunyan-logger')
 const express = require('express')
+const { createServer } = require('http')
+const { execute, subscribe } = require('graphql')
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
 
+const { SubscriptionServer } = require('subscriptions-transport-ws')
+
 const { sequelize } = require('./models')
-const Schema = require('./data/schema')
+const schema = require('./data/schema')
 
 const logger = bunyan.createLogger({
   name: 'Server',
@@ -26,18 +30,20 @@ const app = express()
 app.set('host', APP_HOST)
 app.set('port', APP_PORT)
 
-const engine = new Engine({
-  engineConfig: {
-    apiKey: 'service:ornous-skills-staging:rik0Q78HrR8foBYYjpr46g',
-    logging: {
-      level: 'INFO' // Proxy logging. DEBUG, INFO, WARN or ERROR
-    }
-  },
-  graphqlPort: APP_PORT || 3000, // GraphQL port
-  dumpTraffic: true // Debug configuration that logs traffic between Proxy and GraphQL server
-})
+if (process.env.APP_ENV === 'production') {
+  const engine = new Engine({
+    engineConfig: {
+      apiKey: 'service:ornous-skills-staging:rik0Q78HrR8foBYYjpr46g',
+      logging: {
+        level: 'INFO' // Proxy logging. DEBUG, INFO, WARN or ERROR
+      }
+    },
+    graphqlPort: APP_PORT || 3000, // GraphQL port
+    dumpTraffic: true // Debug configuration that logs traffic between Proxy and GraphQL server
+  })
 
-app.use(engine.expressMiddleware())
+  app.use(engine.expressMiddleware())
+}
 app.use(expressBunyan())
 app.use(compression())
 app.use(
@@ -52,13 +58,19 @@ app.use(
   '/graphql',
   bodyParser.json(),
   graphqlExpress({
-    schema: Schema,
+    schema,
     context: {},
     tracing: true,
     cacheControl: true
   })
 )
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+app.use(
+  '/graphiql',
+  graphiqlExpress({
+    endpointURL: '/graphql',
+    subscriptionsEndpoint: `ws://${APP_HOST}:${APP_PORT}/subscriptions`
+  })
+)
 
 // Graceful startup/shutdown
 app.get('/healthz', (req, res) => {
@@ -78,6 +90,7 @@ app.get('/healthz', (req, res) => {
 
 app.on('listening', () => {
   logger.info('Express server started on port %s at %s', APP_PORT, APP_HOST)
+  logger.info('Serving subscriptions ws endpoint at /subscriptions')
 })
 
 app.on('close', function () {
@@ -97,8 +110,18 @@ sequelize
     logger.info('Attempting to connect to the database')
     sequelize.sync()
 
-    engine.start()
-    app.listen(APP_PORT, APP_HOST, () => app.emit('listening'))
+    if (process.env.APP_ENV === 'production') {
+      engine.start()
+    }
+    const server = createServer(app)
+    server.listen(APP_PORT, APP_HOST, () => {
+      app.emit('listening')
+      const srv = new SubscriptionServer(
+        { execute, subscribe, schema },
+        { server: server, path: '/subscriptions' }
+      )
+      return srv
+    })
   })
   .catch(err => {
     logger.error(
